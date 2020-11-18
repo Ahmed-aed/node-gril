3709f18c8f445c1eb70156355e7d80fcecedb2e9
Ahmed-aed <aeid767@gmail.com>
2020-11-18 11:40:56 +0200

    fix error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const http = require("http");
 const { router } = require("./router");
 const server = http.createServer(router);
-const PORT = 8300;
+const PORT = process.env.PORT||8300;
 server.listen(PORT, () => console.log(`Connected Successfully @ ${PORT} `));
